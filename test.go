fbd3ae6f6b07d5cfbcfe3964ac4026f60fc03596
DrJacob01 <119816008+DrJacob01@users.noreply.github.com>
2023-08-03 15:41:12 +0100

    My First Go Script

---
+++ test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+var name string = "Jacob"
+var catName = "Gin"
+var myNum int = 1
+
+func main() {
+	fmt.Println("Hello", name)
+	fmt.Println("Your cat's name is", catName+"!")
+	print(myNum)
+}
