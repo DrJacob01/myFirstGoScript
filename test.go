a5fb9c6238cad6971e4ba032d0cee1c1aedb08f3
DrJacob01 <119816008+DrJacob01@users.noreply.github.com>
2023-08-04 16:27:54 +0100

    Add files via upload

--- test.go
+++ test.go
@@ -11,5 +11,10 @@ var myNum int = 1
 func main() {
 	fmt.Println("Hello", name)
 	fmt.Println("Your cat's name is", catName+"!")
-	print(myNum)
+	fmt.Println(myNum)
+	fmt.Println("*************************")
+
+	fmt.Println("If anyone ever finds this, this was written by Jacob Harrod on his work experience on 03/08/23 - my first bit of GOlang code! :) ")
+
+	fmt.Println("*************************")
 }
